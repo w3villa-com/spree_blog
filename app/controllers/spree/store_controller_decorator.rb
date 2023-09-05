8ee88444b4fcabf0955d53f43d5f8f29e111dc11
Ravinder Kumar <ravinder.kumar@w3villa.com>
2023-09-06 00:26:56 +0530

    Fix zetwik error

--- app/controllers/spree/store_controller_decorator.rb
+++ app/controllers/spree/store_controller_decorator.rb
@@ -1,3 +1,8 @@
-Spree::StoreController.class_eval do 
-  helper 'spree/blog_entries'
+module Spree::StoreControllerDecorator
+
+  def self.prepended(base)
+    base.helper 'spree/blog_entries'
+  end
+
 end
+::Spree::StoreController.prepend Spree::StoreControllerDecorator if ::Spree::StoreController.included_modules.exclude?(Spree::StoreControllerDecorator)
