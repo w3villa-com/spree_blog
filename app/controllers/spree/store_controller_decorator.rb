4ddcbda71678955e572e7e2fb70d354ab066fa16
Ravinder Kumar <ravinder.kumar@w3villa.com>
2023-09-06 11:18:49 +0530

    Revert changes

--- app/controllers/spree/store_controller_decorator.rb
+++ app/controllers/spree/store_controller_decorator.rb
@@ -1,8 +1,3 @@
-module Spree::StoreControllerDecorator
-
-  def self.prepended(base)
-    base.helper 'spree/blog_entries'
-  end
-
-end
-::Spree::StoreController.prepend Spree::StoreControllerDecorator if ::Spree::StoreController.included_modules.exclude?(Spree::StoreControllerDecorator)
+# Spree::StoreController.class_eval do 
+#   helper 'spree/blog_entries'
+# end
