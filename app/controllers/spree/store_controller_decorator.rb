150c168a7bf1b90b16e45af0dc5ff3acc205a71d
Ravinder Kumar <ravinder.kumar@w3villa.com>
2023-09-06 10:47:50 +0530

    Fix zetvik issue

--- app/controllers/spree/store_controller_decorator.rb
+++ app/controllers/spree/store_controller_decorator.rb
@@ -1,3 +1,8 @@
-Spree::Admin::StoresController.class_eval do 
-  helper 'spree/blog_entries'
+module Spree::StoreControllerDecorator
+
+  def self.prepended(base)
+    base.helper 'spree/blog_entries'
+  end
+
 end
+::Spree::StoreController.prepend Spree::StoreControllerDecorator if ::Spree::StoreController.included_modules.exclude?(Spree::StoreControllerDecorator)
